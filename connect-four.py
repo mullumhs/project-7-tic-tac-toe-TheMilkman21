c8ef0f2278ed5bec0adf4dc434cb0d827e9a6837
TheMilkman21 <140124197+TheMilkman21@users.noreply.github.com>
2024-04-02 04:00:43 +0000

    yes

--- connect-four.py
+++ connect-four.py
@@ -1,5 +1,25 @@
 def main():
-    # Your code goes here
+    board = initialiseBoard()
+    current_player = "Y"
+
+    while True:
+        
+    
+def initialiseBoard():
+    board = []
+    for _ in range(6):
+        row = []
+        for _ in range(7):
+            row.append('-')
+        board.append(row)
+    return board
+    
+def displayBoard(board):
+    for row in board:
+        for cell in row:
+            print(cell, end=' ')
+        print()
+    print()
 
 if __name__ == "__main__":
     main()
