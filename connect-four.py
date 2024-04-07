fd786f764f0cfd1c1ce66700b4cbb547b8ea8941
TheMilkman21 <140124197+TheMilkman21@users.noreply.github.com>
2024-04-07 23:48:19 +0000

    win check

--- connect-four.py
+++ connect-four.py
@@ -5,10 +5,30 @@ for i in range(6):
 
 
 while True:
-    choice = int(input("where do you want to go: "))
-    bord[5][choice] = "X"
-
     for row in bord:
         for col in row:
             print(col, end=" ")
         print()
+
+    choice = int(input("where do you want to go: "))
+    bord[5][choice] = "X"
+
+    
+
+    for row in range(6):
+        for col in range(4):
+            if bord[row][col] == bord[row][col + 1] == bord[row][col + 2] == bord[row][col + 3] and not bord[row][col + 3] == '-':
+                print("win")
+                break
+
+    
+
+
+
+
+
+
+
+
+
+
