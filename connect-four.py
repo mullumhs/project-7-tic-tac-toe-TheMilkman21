02162aa47ed0db0b6782ffab9c7a54f35761b168
TheMilkman21 <140124197+TheMilkman21@users.noreply.github.com>
2024-04-10 00:41:06 +0000

    win check

--- connect-four.py
+++ connect-four.py
@@ -1,10 +1,10 @@
 bord = []
 def intil_bord():
     for i in range(6):
-        bord.append(['-','-','-','-','-','-','-'])
+        bord.append([' - ',' - ' ,' - ',' - ',' - ',' - ',' - '])
 
 def print_bord():
-    print('|1|2|3|4|5|6|7|')
+    print('| 1 | 2 | 3 | 4 | 5 | 6 | 7 |')
     for row in bord:
         print('', end='|')
         for col in row:
@@ -19,25 +19,42 @@ while True:
     token = ' X '
     if PlayerCounter % 2 == 0:
         token = ' O '
+    
+    #player input
 
     print('')
     choice = int(input("where do you want to go: "))
     print('')
     choice -= 1
 
-    
-    for row in range(6):
-        for col in range(4):
-            if bord[row][col] == bord[row][col + 1] == bord[row][col + 2] == bord[row][col + 3] and not bord[row][col + 3] == '-':
-                print("win")
-                break
-    
-    
+    #place token
     for i in range (5, -1, -1):
         if bord[i][choice] == ' - ':
             bord[i][choice] = token
             PlayerCounter += 1
             break
+
+
+    #across
+    for row in range(6):
+        for col in range(4):
+            if bord[row][col] == bord[row][col + 1] == bord[row][col + 2] == bord[row][col + 3] and not bord[row][col + 3] == ' - ':
+                print("win")
+                break
+    #up
+    for row in range(3):
+        for col in range(6):
+             if bord[row][col] == bord[row + 1][col] == bord[row + 2][col] == bord[row + 3][col] and not bord[row + 3][col] == ' - ':
+                print("win")
+                break
+    #diaginal   
+    for row in range(3):
+            for col in range(6):
+                if bord[row][col] == bord[row + 1][col + 1] == bord[row + 2][col + 2] == bord[row + 3][col + 3] and not bord[row + 3][col + 3] == ' - ':
+                    print("win")
+                    break
+    
+    
     print_bord()
 
 
