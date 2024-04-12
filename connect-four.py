a87bf23cef74dd5a21a49e1037947abb5d7f7ded
TheMilkman21 <140124197+TheMilkman21@users.noreply.github.com>
2024-04-12 04:09:18 +0000

    stat on tic tac toe

--- connect-four.py
+++ connect-four.py
@@ -5,6 +5,7 @@ def intil_bord():
     
 
 def print_bord():
+    print(' ___ ___ ___ ___ ___ ___ ___ ')
     print('| 1 | 2 | 3 | 4 | 5 | 6 | 7 |')
     for row in bord:
         print('', end='|')
@@ -34,15 +35,15 @@ def vertical_check():
 def diagonal_check(): 
     #diagonal   
     for row in range(3):
-            for col in range(4):
-                # diag L
-                if bord[row][col] == bord[row + 1][col + 1] == bord[row + 2][col + 2] == bord[row + 3][col + 3] and not bord[row + 3][col + 3] == ' - ':
-                    print("win")
-                    break
-                # diag R
-                elif bord[row][col] == bord[row + 1][col - 1] == bord[row + 2][col - 2] == bord[row + 3][col - 3] and not bord[row + 3][col - 3] == ' - ':
-                    print("win")
-                    break
+        for col in range(4):
+            # diag L
+            if bord[row][col] == bord[row + 1][col + 1] == bord[row + 2][col + 2] == bord[row + 3][col + 3] and not bord[row + 3][col + 3] == ' - ':
+                print("win")
+                break
+            # diag R
+            elif bord[row][col] == bord[row + 1][col - 1] == bord[row + 2][col - 2] == bord[row + 3][col - 3] and not bord[row + 3][col - 3] == ' - ':
+                print("win")
+                break
 
 PlayerCounter = 1 
 intil_bord()
--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,5 +1,46 @@
-def main():
-    # Your code goes here
+bord = []
+def intil_bord():
+    for i in range(3):
+        bord.append([' - ',' - ' ,' - ',])
 
-if __name__ == "__main__":
-    main()
+def print_bord():
+    print(' ___ ___ ___ ')
+    print('| 1 | 2 | 3 |')
+    for row in bord:
+        print('', end='|')
+        for col in row:
+            print(col, end='|')
+        print()
+        horisontal_check()
+
+def horisontal_check():
+    #across
+    for row in range(2):
+        for col in range(1):
+            if bord[row][col] == bord[row][col + 1] == bord[row][col + 2]  and not bord[row][col + 2] == ' - ':
+                print("win")
+                break
+
+PlayerCounter = 1 
+intil_bord()
+print_bord()
+
+
+while True:
+    token = ' X '
+    if PlayerCounter % 2 == 0:
+        token = ' O '
+
+
+    print('')
+    choice = int(input("where do you want to go: "))
+    print('')
+    choice -= 1
+
+    for i in range (2, -1, -1):
+        if bord[i][choice] == ' - ':
+            bord[i][choice] = token
+            PlayerCounter += 1
+            break
+
+    print_bord()
