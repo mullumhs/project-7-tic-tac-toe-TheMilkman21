fab55c3c8810f2ed0915d8942ba53cc147b7bf32
TheMilkman21 <140124197+TheMilkman21@users.noreply.github.com>
2024-05-03 00:41:13 +0000

    work

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -15,8 +15,8 @@ def print_bord():
 
 def horisontal_check():
     #across
-    for row in range(1):
-        if bord[0][row] == bord[1][row] == bord[2][row]  and not bord[2][row] == ' - ':
+    for row in range(3):
+        if bord[0][row] == bord[1][row] == bord[2][row] and not bord[2][row] == ' - ':
             print("win hori")
             break
 
@@ -27,7 +27,11 @@ def vertical_check():
             print("win vert")
             break
 
-
+def diag_check():
+    if bord[0][0] == bord[1][1] == bord[2][2] and not bord[0][0] == ' - ':
+        print("win")
+    if bord[0][0] == bord[1][1] == bord[2][2] and not bord[0][0] == ' - ':
+        print("win")
 
 PlayerCounter = 1 
 intil_bord()
@@ -42,22 +46,27 @@ while True:
         print("Player 2's go")
         token = ' O '
 
-
+  
     print('')
     choice_row = int(input("which row do you want to go(1,2,3): ")) - 1
     choice_col = int(input("which colum do you want to go(1,2,3): ")) - 1
     print('')
-
-
+       
     
+    if (choice_col >= 0 and choice_col < 3) and (choice_row >= 0 and choice_row < 3):
 
-
+        if bord[choice_col][choice_row] == ' - ':
+            bord[choice_col][choice_row] = token
+            PlayerCounter += 1
+        else:
+            print("error thing there")
+    else:
+        print("error not a place")
     
-    bord[choice_col][choice_row] = token 
-    PlayerCounter += 1
     
     
 
     print_bord()
     horisontal_check()
     vertical_check()
+    diag_check()
